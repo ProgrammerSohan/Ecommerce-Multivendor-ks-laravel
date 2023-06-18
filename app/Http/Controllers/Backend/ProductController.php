6f17047ed8deaf7e7bd84d015d3185d375401e77
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-06-18 22:29:28 +0600

    T186-Product active inactive

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -227,5 +227,34 @@ class ProductController extends Controller
         }// end method
 
 
+        public function ProductInactive($id){
+
+            Product::findOrFail($id)->update(['status'=>0]);
+            $notification = array(
+                'message' => 'Product Inactive',
+                'alert-type' => 'success'
+
+            );
+
+            return redirect()->back()->with($notification);
+
+        }// end method
+
+        public function ProductActive($id){
+
+            Product::findOrFail($id)->update(['status' => 1]);
+            $notification = array(
+                'message' => 'Product Active',
+                'alert-type' => 'success'
+
+            );
+
+            return redirect()->back()->with($notification);
+
+        }// end method
+
+
+
+
 
 }
--- routes/web.php
+++ routes/web.php
@@ -148,6 +148,9 @@ Route::controller(CategoryController::class)->group(function(){
         Route::post('/update/product/multiimage', 'UpdateProductMultiimage')->name('update.product.multiimage');
         Route::get('/product/multiimg/delete/{id}', 'MultiImageDelete')->name('product.multiimg.delete');
 
+        Route::get('/product/inactive/{id}', 'ProductInactive')->name('product.inactive');
+        Route::get('/product/active/{id}', 'ProductActive')->name('product.active');
+
     });// end method
 
 });// end middleware

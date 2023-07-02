a9b4d3da53e0b33a9c3ca54e04fda3cac42e3453
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-02 19:55:53 +0600

    193 add product from vendor db & page design part05

--- resources/views/vendor/backend/product/vendor_product_edit.blade.php
+++ resources/views/vendor/backend/product/vendor_product_edit.blade.php
@@ -180,7 +180,7 @@
     <h6 class="mb-0 text-uppercase">Update Main Image Thambnail</h6>
     <hr>
 <div class="card">
-    <form method="post" action="{{route('update.product.thambnail')}}" enctype="multipart/form-data">
+    <form method="post" action="{{route('vendor.update.product.thambnail')}}" enctype="multipart/form-data">
         @csrf
         <input type="hidden" name="id" value="{{ $products->id}}">
         <input type="hidden" name="old_img" value="{{ $products->product_thambnail}}">
@@ -223,7 +223,7 @@
             </thead>
             <tbody>
 
-                <form method="post" action="{{route('update.product.multiimage')}}" enctype="multipart/form-data">
+                <form method="post" action="{{route('vendor.update.product.multiimage')}}" enctype="multipart/form-data">
                     @csrf
 
           @foreach ($multiImgs as $key => $img)
@@ -234,7 +234,7 @@
                     <td> <input type="file" class="form-group" name="multi_img[{{ $img->id }}]"></td>
                     <td>
                         <input type="submit" class="btn btn-primary px-4" value="Update Image"/>
-              <a href="{{ route('product.multiimg.delete',$img->id) }}" class="btn btn-danger" id="delete">Delete</a>
+              <a href="{{ route('vendor.product.multiimg.delete',$img->id) }}" class="btn btn-danger" id="delete">Delete</a>
                     </td>
                 </tr>
 

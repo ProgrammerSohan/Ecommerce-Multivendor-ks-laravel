2bd81fcdcdb550909a8d9a00838617f1d82f11e3
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-23 04:32:14 +0600

    207_frontend dynamic new product display part1_s30

--- resources/views/backend/product/product_edit.blade.php
+++ resources/views/backend/product/product_edit.blade.php
@@ -24,7 +24,7 @@
   <div class="card-body p-4">
 	  <h5 class="card-title">Edit Product</h5>
 	  <hr/>
-      <form id="myForm" method="post" action="{{ route('update.product') }}" >
+      <form id="myForm" method="post" action="{{ route('update.product') }}" enctype="multipart/form-data">
         @csrf
         <input type="hidden" name="id" value="{{ $products->id }}">
        <div class="form-body mt-4">

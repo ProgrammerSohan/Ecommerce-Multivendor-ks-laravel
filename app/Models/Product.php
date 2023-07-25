e1d317145db5418e8f948cfd2e3f431910fa3fb0
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-25 22:07:41 +0600

    208_frontend dynamic new product display part2_s30

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,11 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function vendor(){
+        return $this->belongsTo(User::class,'vendor_id','id');
+
+    }
+
+    public function category(){
+
+        return $this->belongsTo(Category::class,'category_id','id');
+    }
+
+
 }

d5cdeafabde611184baf87b8de499b86f2e78d06
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-20 05:30:44 +0600

    232_product view modal with ajax part2#35

--- resources/views/frontend/master_dashboard.blade.php
+++ resources/views/frontend/master_dashboard.blade.php
@@ -6,6 +6,9 @@
     <title>Sohan Online Shop</title>
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <meta name="description" content="" />
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta property="og:title" content="" />
     <meta property="og:type" content="" />
@@ -73,6 +76,18 @@
     <!-- Template  JS -->
     <script src="{{ asset('frontend/assets/js/main.js?v=5.3') }}"></script>
     <script src="{{ asset('frontend/assets/js/shop.js?v=5.3') }}"></script>
+
+    <script type="text/javascript">
+        $.ajaxSetup({
+            headers:{
+                'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('content')
+            }
+        })
+
+        ///start product view with modal
+
+    </script>
+
 </body>
 
 </html>

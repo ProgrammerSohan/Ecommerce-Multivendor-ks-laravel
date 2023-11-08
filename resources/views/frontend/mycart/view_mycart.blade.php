c426dd639fcd7702eeb113736b3aa677fbf31e25
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-11-09 05:55:42 +0600

    _58.2_cartpage design setup(b)

--- resources/views/frontend/mycart/view_mycart.blade.php
+++ resources/views/frontend/mycart/view_mycart.blade.php
@@ -6,7 +6,7 @@
     <div class="container">
         <div class="breadcrumb">
             <a href="index.html" rel="nofollow"><i class="fi-rs-home mr-5"></i>Home</a>
-            <span></span> Shop
+           
             <span></span> Cart
         </div>
     </div>
@@ -16,8 +16,8 @@
         <div class="col-lg-8 mb-40">
             <h1 class="heading-2 mb-10">Your Cart</h1>
             <div class="d-flex justify-content-between">
-                <h6 class="text-body">There are <span class="text-brand">3</span> products in your cart</h6>
-                <h6 class="text-body"><a href="#" class="text-muted"><i class="fi-rs-trash mr-5"></i>Clear Cart</a></h6>
+                <h6 class="text-body">There are products in your cart</h6>
+            
             </div>
         </div>
     </div>
@@ -28,11 +28,12 @@
                     <thead>
                         <tr class="main-heading">
                             <th class="custome-checkbox start pl-30">
-                                <input class="form-check-input" type="checkbox" name="checkbox" id="exampleCheckbox11" value="">
-                                <label class="form-check-label" for="exampleCheckbox11"></label>
+                                
                             </th>
                             <th scope="col" colspan="2">Product</th>
                             <th scope="col">Unit Price</th>
+                            <th scope="col">Color</th>
+                            <th scope="col">Size</th>
                             <th scope="col">Quantity</th>
                             <th scope="col">Subtotal</th>
                             <th scope="col" class="end">Remove</th>
@@ -41,8 +42,8 @@
                     <tbody>
                         <tr class="pt-30">
                             <td class="custome-checkbox pl-30">
-                                <input class="form-check-input" type="checkbox" name="checkbox" id="exampleCheckbox1" value="">
-                                <label class="form-check-label" for="exampleCheckbox1"></label>
+                                
+
                             </td>
                             <td class="image product-thumbnail pt-40"><img src="assets/imgs/shop/product-1-1.jpg" alt="#"></td>
                             <td class="product-des product-name">
@@ -58,6 +59,13 @@
                             <td class="price" data-title="Price">
                                 <h4 class="text-body">$2.51 </h4>
                             </td>
+                            <td class="price" data-title="Price">
+                                <h6 class="text-body">Color</h6>
+                            </td>
+                            <td class="price" data-title="Price">
+                                <h6 class="text-body">Size</h6>
+                            </td>
+
                             <td class="text-center detail-info" data-title="Stock">
                                 <div class="detail-extralink mr-15">
                                     <div class="detail-qty border radius">
@@ -72,72 +80,7 @@
                             </td>
                             <td class="action text-center" data-title="Remove"><a href="#" class="text-body"><i class="fi-rs-trash"></i></a></td>
                         </tr>
-                        <tr>
-                            <td class="custome-checkbox pl-30">
-                                <input class="form-check-input" type="checkbox" name="checkbox" id="exampleCheckbox2" value="">
-                                <label class="form-check-label" for="exampleCheckbox2"></label>
-                            </td>
-                            <td class="image product-thumbnail"><img src="assets/imgs/shop/product-2-1.jpg" alt="#"></td>
-                            <td class="product-des product-name">
-                                <h6 class="mb-5"><a class="product-name mb-10 text-heading" href="shop-product-right.html">Blue Diamond Almonds Lightly Salted</a></h6>
-                                <div class="product-rate-cover">
-                                    <div class="product-rate d-inline-block">
-                                        <div class="product-rating" style="width:90%">
-                                        </div>
-                                    </div>
-                                    <span class="font-small ml-5 text-muted"> (4.0)</span>
-                                </div>
-                            </td>
-                            <td class="price" data-title="Price">
-                                <h4 class="text-body">$3.2 </h4>
-                            </td>
-                            <td class="text-center detail-info" data-title="Stock">
-                                <div class="detail-extralink mr-15">
-                                    <div class="detail-qty border radius">
-                                        <a href="#" class="qty-down"><i class="fi-rs-angle-small-down"></i></a>
-                                        <input type="text" name="quantity" class="qty-val" value="1" min="1">
-                                        <a href="#" class="qty-up"><i class="fi-rs-angle-small-up"></i></a>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="price" data-title="Price">
-                                <h4 class="text-brand">$3.2 </h4>
-                            </td>
-                            <td class="action text-center" data-title="Remove"><a href="#" class="text-body"><i class="fi-rs-trash"></i></a></td>
-                        </tr>
-                        <tr>
-                            <td class="custome-checkbox pl-30">
-                                <input class="form-check-input" type="checkbox" name="checkbox" id="exampleCheckbox3" value="">
-                                <label class="form-check-label" for="exampleCheckbox3"></label>
-                            </td>
-                            <td class="image product-thumbnail"><img src="assets/imgs/shop/product-3-1.jpg" alt="#"></td>
-                            <td class="product-des product-name">
-                                <h6 class="mb-5"><a class="product-name mb-10 text-heading" href="shop-product-right.html">Fresh Organic Mustard Leaves Bell Pepper</a></h6>
-                                <div class="product-rate-cover">
-                                    <div class="product-rate d-inline-block">
-                                        <div class="product-rating" style="width:90%">
-                                        </div>
-                                    </div>
-                                    <span class="font-small ml-5 text-muted"> (4.0)</span>
-                                </div>
-                            </td>
-                            <td class="price" data-title="Price">
-                                <h4 class="text-body">$2.43 </h4>
-                            </td>
-                            <td class="text-center detail-info" data-title="Stock">
-                                <div class="detail-extralink mr-15">
-                                    <div class="detail-qty border radius">
-                                        <a href="#" class="qty-down"><i class="fi-rs-angle-small-down"></i></a>
-                                        <input type="text" name="quantity" class="qty-val" value="1" min="1">
-                                        <a href="#" class="qty-up"><i class="fi-rs-angle-small-up"></i></a>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="price" data-title="Price">
-                                <h4 class="text-brand">$2.43 </h4>
-                            </td>
-                            <td class="action text-center" data-title="Remove"><a href="#" class="text-body"><i class="fi-rs-trash"></i></a></td>
-                        </tr>
+                      
                     </tbody>
                 </table>
             </div>

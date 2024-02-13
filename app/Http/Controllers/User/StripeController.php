89dd20a876525173a4a3753b327f044e0e13b80b
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-02-13 15:54:35 +0600

    _92_stripe payment gateway part1

--- app/Http/Controllers/User/StripeController.php
+++ app/Http/Controllers/User/StripeController.php
@@ -9,6 +9,18 @@ class StripeController extends Controller
 {
      public function StripeOrder(){
 
+            \Stripe\Stripe::setApiKey('sk_test_51OjENQD6fI3thGcmRSinbYZE0LMY6jitwaIGYrfwzT09K7PryxFlJ4MkCIB7hYjbjYOUnfAGfzvmYi4wl3B8Uqw6001NhJfi9s');
+
+            $token = $_POST['stripeToken'];
+            $charge = \Stripe\Charge::create([
+            'amount' => 999*100,
+            'currency' => 'usd',
+            'description' => 'Sohan Online Shop',
+            'source' => $token,
+            'metadata' => ['order_id' => '6735']
+            ]);
+
+            dd($charge);
 
      } //end method
 }

caa7fa8d653574fd6813385884d16e97b2547a92
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-02-19 12:37:46 +0600

    _303_user dashboard design & setup part01_c

--- app/Http/Controllers/User/AllUserController.php
+++ app/Http/Controllers/User/AllUserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AllUserController extends Controller
 {
